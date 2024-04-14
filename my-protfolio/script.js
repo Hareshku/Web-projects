a52ab8df584cefc4b3d1a3f33d0ac54d9d612bed
Hareshku <hareesh7737@gmail.com>
2024-04-14 17:33:57 +0500

    Some code in menu bar updated

--- my-protfolio/script.js
+++ my-protfolio/script.js
@@ -17,3 +17,14 @@ window.addEventListener("scroll",() =>{
         toTop.classList.remove("active");
     }
 })
+
+
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.header');
+    if (window.scrollY > 100) {
+      navbar.style.backgroundColor = '#175d69'; // Change to desired color
+    } else {
+      navbar.style.backgroundColor = ''; // Reset to initial color
+    }
+  });
